47ddd6f5cd50e9a30761d6e092405640fa01cec2
VinBaz <67991028+vinaybaswa@users.noreply.github.com>
2025-01-21 04:48:04 -0500

    build NavMenu component

---
+++ app/NavMenu.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./NavMenu.module.css";
+
+export default function NavMenu() {
+  return (
+    <nav className={styles.nav}>
+      <Link href="/">
+        <Image src="/logo.svg" alt="logo" width={216} height={30} />
+      </Link>
+      <ul className={styles.links}>
+        <li>
+          <Link href={"/about"}>About</Link>
+        </li>
+        <li>
+          <Link href={"/blog"}>Blog</Link>
+        </li>
+        <li>
+          <Link href={"/users"}>Users</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
