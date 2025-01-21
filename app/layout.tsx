44f25f42205c33d2426a21649aacdcdf51a2217c
VinBaz <67991028+vinaybaswa@users.noreply.github.com>
2025-01-21 15:53:02 -0500

    setup AuthProvider using SessionProvider from next-auth/react

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavMenu from "./NavMenu";
+import AuthProvider from "./AuthProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +25,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <AuthProvider>
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <NavMenu/>
         {children}
       </body>
     </html>
+    </AuthProvider>
   );
 }
